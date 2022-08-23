403d5bc4f1c9f9c04cb204f55eb22796bc4f8f5c
Mukhammadali <ali_6017@mail.ru>
2022-08-23 21:41:56 +0500

    Add files and deleted files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// import "./App.css";
+import ApiKey from "./ApiKey";
+
+function App() {
+  return (
+    <>
+      <ApiKey />
+      <h1>YouTube CLONE</h1>
+    </>
+  );
+}
+
+export default App;
