30f597b7fb83889f6ca3b0181d8fa9cfca881e38
Mukhammadali <ali_6017@mail.ru>
2022-08-24 18:24:07 +0500

    Add site Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+// Global-Styles
+
 // CONTENT
 import Header from "../pages/Header";
 
@@ -6,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+
       <h1>YouTube CLONE</h1>
     </>
   );
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <div className="header">
-      <a href="/" className="brand">
-        <span><i className="fa-solid fa-bars"></i></span>
-        <i className="fa-brands fa-youtube"></i>
-        <span>YouTube</span>
-      </a>
-      <div className="search_head">
-        <input type="text" placeholder="Search..."/>
-        <i className="fa-solid fa-keyboard"></i>
-        <i className="fa-solid fa-magnifying-glass"></i>
-        <span><i className="fa-solid fa-microphone"></i></span>
-        <div className="icons">
-          <a href="/">
-            <i>5</i>
-          </a>
+      <div className="logo-lfet">
+        <a href="/" className="brand">
+          <span>
+            <i className="fa-solid fa-bars mr-3"></i>
+          </span>
+          <i className="fa-brands fa-youtube"></i>
+          <span>YouTube</span>
+        </a>
+      </div>
+      <div className="input-group  input-group-lg mt-2 w-50">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Введите запрос"
+        />
+        <div className="input-group-append" id="button-addon4">
+          <button className="btn border" type="button">
+            <i className="fa-solid fa-keyboard"></i>
+          </button>
+          <button className="btn btn-light border" type="button">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
         </div>
+        <span className="micfill">
+          <i className="bi bi-mic-fill mt-3"></i>
+        </span>
+      </div>
+      <div className="icons-right">
+        <a href="/">
+          <i class="bi bi-camera-video mr-3"></i>
+          <i className="fas fa-bell mr-3"></i>
+          <i className="fab fa-adn"></i>
+        </a>
       </div>
     </div>
   );
